65bfd0ad17a5a8a336d8c25dae42b48bdf4c700c
Jason Harriot <jasonharriot@gmail.com>
2019-04-17 23:12:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
-$("#send").click(function(){
-    console.log("Send");
-});
+const server = "54.157.206.188";
+
+var lastMessageIndex = -1;	//Index of the latest message in the client's posession
+
+$("#send").click(postMessage);
+setInterval(getMessages, 1000);	//TODO: This is awful... but it works w/o server acknowledgement of clients
+
+function writeMessages(json){
+	var o = $("#output");
+	for(var msg of json){
+		o.append("[" + msg.user + "] " + msg.content + "\n");
+	}
+}
+
+function postMessage(){
+    console.log("Sending...");
+    if(!$("#input").val() || !$("#name").val()){
+    	console.log("Blank fields");
+    	return;
+    }
+    var req = new XMLHttpRequest();
+    req.open("POST", "https://cors-anywhere.herokuapp.com/" + server + "/postMessage");
+    req.setRequestHeader("Content-Type", "application/json");
+    
+    content = {
+    	"content": $("#input").val(),
+    	"user": $("#name").val()
+    }
+    
+    console.log("Sending: " + JSON.stringify(content));
+    req.send(JSON.stringify(content));
+    
+    req.onreadystatechange = function(){
+    	if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
+    		console.log("Success sending message");
+    		$("#input").val("");
+    	}
+    }
+}
+
+function getMessages(){
+	console.log("Getting...");
+	var req = new XMLHttpRequest();
+	req.open("POST", "https://cors-anywhere.herokuapp.com/" + server + "/getMessages?getFrom=" + (lastMessageIndex + 1));
+	req.setRequestHeader("Content-Type", "application/json");
+	
+	req.send();
+	
+	req.onreadystatechange = function(){
+		if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
+			var nextMessages = JSON.parse(this.response);
+			lastMessageIndex += nextMessages.length;
+			writeMessages(nextMessages);
+		}
+	}
+}
