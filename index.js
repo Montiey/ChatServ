310afdb0c5d10837067bfc1cdceb8478dd53b600
root <root@ip-172-31-94-148.ec2.internal>
2019-04-28 17:53:49 +0000

    Fixed bricking, added proper content checks

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const urlParser = require("url");
 const fs = require("fs");
 const port = 80;
 
-/*const auth = {
-	key:  fs.readFileSync("ssl/key.pem"),
-	cert: fs.readFileSync("ssl/cert.pem")
-}*/
-
 var messages = [
 ]
 
@@ -33,39 +28,48 @@ const requestHandler = function(request, response){
 	console.log(getDate() + " [" + request.connection.remoteAddress + "]" + request.url);
 
 	var url = urlParser.parse(request.url, true);
-	
-	response.setHeader("Access-Control-Allow-Origin", "*");
-	response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-	response.setHeader("Access-Control-Allow-Headers", "X-Requested-with, content-type");
 
-	if(url.pathname == "/getMessages"){
-		var fromIndex = url.query.getFrom;
-		var newMessages = messages.slice(fromIndex);
-		response.write(JSON.stringify(newMessages));
-		response.end();
-	} else if(url.pathname == "/postMessage"){
-		var content = "";
-		request.on("data", function(data){
-			content += data;
-			if(content.length > 1e6){
-				request.connection.destroy();
-				console.log("!!! Data overflow !!!");
-			} else{
-			}
-		});
-		request.on("end", function(){
-			console.log("Received msg: " + content);
-			var msg = JSON.parse(content);
-			if(msg.user && msg.content){
-				messages.push(new Message(msg.user, msg.content));
-			} else{
-				console.log("Ignoring blank");
-			}
-		});
-		response.end();
-	} else{
-		fServer.serve(request, response);
-	}
+	response.setHeader("access-control-allow-origin", "*");
+	response.setHeader("access-control-allow-methods", "GET, POST, OPTIONS");
+	response.setHeader("access-control-allow-headers", "x-requested-with, content-type");	
+	
+		if(url.pathname == "/getMessages"){
+			var fromIndex = url.query.getFrom;
+			var newMessages = messages.slice(fromIndex);
+			response.setHeader("content-type", "application/json");
+			response.write(JSON.stringify(newMessages));
+			response.end();
+		} else if(url.pathname == "/postMessage" && request.headers["content-type"] == "application/json"){
+			var content = "";
+			request.on("data", function(data){
+				content += data;
+				if(content.length > 1e6){
+					request.connection.destroy();
+					console.log("!!! Data overflow !!!");
+				} else{
+				}
+			});
+			request.on("end", function(){
+				console.log("Received msg: " + content);
+				var msg = null;
+				try{
+					msg = JSON.parse(content);
+				} catch(e){
+					console.log("JSON: " + e);
+				}
+
+				if(msg){
+					if(msg.user && msg.content){
+						messages.push(new Message(msg.user, msg.content));
+					} else{
+						console.log("Ignoring blank");
+					}
+				}
+			});
+			response.end();
+		} else{
+			response.end();
+		}
 }
 
 //const server = https.createServer(auth, requestHandler);
