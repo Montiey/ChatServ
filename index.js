98fe7060e39fb2bad0ea55ec62e8a8256ad01671
root <root@ip-172-31-94-148.ec2.internal>
2019-05-05 00:47:32 +0000

    Use JSON files for stuff

--- index.js
+++ index.js
@@ -2,38 +2,62 @@ const http = require("http");
 const nStatic = require("node-static");
 const urlParser = require("url");
 const fs = require("fs");
+const jsonfile = require("jsonfile");
+
 const port = 80;
+const qrFile = "qrref.json";
+const messageFile = "messages.json";
 
-var messages = [
-]
-
-function Message(user, content){
-	this.content = content;
-	this.user = user;
-}
-
-messages.push(new Message("Welcome", "to ChatServ"));
-
-function getDate(){
+function getTimestamp(){
 	var d = new Date();
-	return (d.getMonth() + 1) + "-" + d.getDate() + "-" + d.getFullYear();
+	return (d.getMonth() + 1) + "-" + d.getDate() + "-" + d.getFullYear() + " " + (d.getHours() < 10 ? "0" + d.getHours() : d.getHours()) + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes());
 }
+
+function editJSON(path, callback){
+	path = "config/" + path;
+	var content;
+
+	if(fs.existsSync(path)){
+		content = JSON.parse(fs.readFileSync(path));
+	} else{
+		console.log("Update: File doesn't exist");
+		return;
+	}
+
+	callback(content);
+	fs.writeFileSync(path, JSON.stringify(content, null, 4));
+}
+
+function readJSON(path){
+	path = "config/" + path;
+	
+	if(fs.existsSync(path)){
+		return JSON.parse(fs.readFileSync(path));
+	} else{
+		console.log("Read: File doesn't exist");
+		return;
+	}
+}
+
 
 const fServer = new nStatic.Server("./pub"); //Hot fresh HTML
 
 const requestHandler = function(request, response){
-	console.log(getDate() + " [" + request.connection.remoteAddress + " T: " + request.headers["content-type"] + "]" + request.url);
+	console.log(getTimestamp() + " [" + request.connection.remoteAddress + " T: " + request.headers["content-type"] + "]" + request.url);
 
 	try{
 		var url = urlParser.parse(request.url, true);
-
+		
+		
 		response.setHeader("access-control-allow-origin", "*");
 		response.setHeader("access-control-allow-methods", "GET, POST, OPTIONS");
 		response.setHeader("access-control-allow-headers", "x-requested-with, content-type");	
 		
+
+
 		if(url.pathname == "/getMessages"){
 			var fromIndex = url.query.getFrom;
-			var newMessages = messages.slice(fromIndex);
+			var newMessages = readJSON(messageFile).list.slice(fromIndex);
 			response.setHeader("content-type", "application/json");
 			response.write(JSON.stringify(newMessages));
 			response.end();
@@ -58,7 +82,12 @@ const requestHandler = function(request, response){
 
 				if(msg){
 					if(msg.user && msg.content){
-						messages.push(new Message(msg.user, msg.content));
+						editJSON(messageFile, function(json){
+							json.list.push({
+								user: msg.user,
+								content: msg.content
+							});
+						});
 					} else{
 						console.log("Ignoring blank");
 					}
@@ -66,20 +95,44 @@ const requestHandler = function(request, response){
 			});
 			response.end();
 		} else{
-			fServer.serve(request, response);
-		
+			if(url.query.ref == "qr"){		
+				editJSON(qrFile, function(json){
+					var exists = false;
+					for(var entry of json.list){
+						console.log("Comparing " + entry.address + " - " + request.connection.remoteAdress);
+						if(entry.address == request.connection.remoteAddress){
+							exists = true;
+							entry.count++;
+							break;
+						}
+					}
+					if(!exists){
+						json.list.push({
+							address: request.connection.remoteAddress,
+							count: 0
+						});
+					}
+					
+				});
+				response.writeHead(302, {
+					"location": url.pathname	//Remove query string
+				});
+				response.end();
+			} else{
+				fServer.serve(request, response);
+			}
 		}
 	} catch(e){
 		console.log("!!!!!!!! Couldn't respond to request: " + e);
 	}
 }
 
-//const server = https.createServer(auth, requestHandler);
 const server = http.createServer(requestHandler);
 server.listen(port);
 
 
 process.on("SIGINT", () => {
 	console.log("SIGINT - restarting");
+	process.exit(0);
 });
 console.log("ChatServ started on port " + port);
